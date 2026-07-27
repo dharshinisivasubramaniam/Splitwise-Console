c86bc3d5a2c4e6e1b7b332d6597654d6dfb66251
Dharshini sivasubramaniam <231696172+dharshinisivasubramaniam@users.noreply.github.com>
2026-07-27 19:14:34 +0530

    SplitwiseApp1

---
+++ src/splitwise_console/SplitwiseApp.java
@@ -0,0 +1,12 @@
+package splitwise_console;
+
+public class SplitwiseApp
+{
+    public static void main(String[] args)
+    {
+      System.out.println("=== Splitwise Console ===");
+      System.out.println("Track shared expenses with friends.");
+      
+    }
+}
+
