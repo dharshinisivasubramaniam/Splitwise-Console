1436304061c06083abd205187c677f33fedd9d23
Dharshini sivasubramaniam <231696172+dharshinisivasubramaniam@users.noreply.github.com>
2026-07-29 16:29:51 +0530

    Commit of 1.3

--- src/splitwise_console/SplitwiseApp.java
+++ src/splitwise_console/SplitwiseApp.java
@@ -8,10 +8,14 @@ public class SplitwiseApp
       System.out.println("Track shared expenses with friends.");
       System.out.println();
       System.out.println("Ready. More features coming in the next lessons.");
+      String payerName = "Dharsh";
       double totalAmount=500.0;
       int numFriends=3;
+        System.out.println(payerName);
         System.out.println(totalAmount);
         System.out.println(numFriends);
+
+
 
 
       
